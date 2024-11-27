5e9b618379879c29754f44761857fa604a971115
Vicente Antonio Monsalve Vargas <vicente.monsalve@gmail.com>
2024-11-27 13:58:13 -0300

    add .gitignore

--- obj/Debug/net8.0/ENTITY_FRAMEWORK_EXAMPLE.AssemblyInfo.cs
+++ obj/Debug/net8.0/ENTITY_FRAMEWORK_EXAMPLE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ENTITY_FRAMEWORK_EXAMPLE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba5bc133acbbc5da1af61bd00bde2c2c14eef52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a9bb2e4116ec4bdcedf4dad917c492795fbad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ENTITY_FRAMEWORK_EXAMPLE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ENTITY_FRAMEWORK_EXAMPLE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
